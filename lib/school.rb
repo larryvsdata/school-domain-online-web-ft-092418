f7185da4e38c94b2a0dc28049a9e646b2d9abaa1
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-29 04:38:48 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,15 @@ class School
     @name = name
     @roster={}
   end
- 
+ def add_student(name,grade)
+   
+   if @roster.keys.include?(grade)
+     @roster[grade]<<name 
+   else 
+   
+   end
+   
+ end 
 
  
 end
