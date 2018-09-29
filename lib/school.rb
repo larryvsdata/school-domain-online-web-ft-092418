0a5612d9079d4ca15687808b87155d4c8b271ca4
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-29 04:25:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,10 @@
-class school
+class School
   attr_accessor :author, :page_count, :genre
  
   def initialize(title)
     @title = title
   end
  
-  def title
-    @title
-  end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+
  
 end
